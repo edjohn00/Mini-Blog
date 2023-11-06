b27baae055b1dc6442105a7be2a9a3cb62fc5086
John Eddie <johneddieablao99@gmail.com>
2023-11-06 16:19:02 +0800

    done with ticket-6 install htmx and create basic htmxResponseMixin

---
+++ app/local_settings.py
@@ -0,0 +1,6 @@
+DATABASE_PASSWORD = "admin"
+DATABASE_PORT = 5433
+DATABASE_NAME = "assessment2"
+DATABASE_USER = "postgres"
+DATABASE_ENGINE = "django.db.backends.postgresql"
+DATABASE_HOST = "localhost"
---
+++ app/views/mixin.py
@@ -0,0 +1,37 @@
+from django.urls import resolve
+from django.views.generic.base import TemplateResponseMixin, ContextMixin
+from typing import Sequence
+
+
+class TemplateLocationMixin(TemplateResponseMixin):
+    template_location = None
+
+    def get_namespace(self) -> str:
+        return resolve(self.request.path).namespace
+
+    def get_template_location(self) -> str:
+        namespace = self.get_namespace()
+        return namespace.replace(":", "/")
+
+    def get_templates_names(self) -> Sequence[str]:
+        return [self.get_template()]
+
+    def get_base_location(self) -> str:
+        namespace = self.get_namespace()
+        return "/".join(namespace.split(":")[0:2])
+
+    def get_template(self) -> str:
+        if self.template_location:
+            return f"{ self.template_location }/{ self.template_name }"
+        else:
+            return f"{ self.get_template_location() }/{ self.template_name }"
+
+
+class HtmxResponseMixin(TemplateLocationMixin, ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.htmx:
+            context["base_template"] = f"{self.get_base_location()}/partial.html"
+        else:
+            context["base_template"] = f"{self.get_base_location()}/base.html"
+        return context
